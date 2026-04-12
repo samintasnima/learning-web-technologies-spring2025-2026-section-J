ba5b4195aa18ecccc5f26309d97a1264611af14c
Samin Tasnima Tisha <samintasnima@gmail.com>
2026-04-12 14:32:15 +0600

    folder

---
+++ new_folder_html/Final lab task/lab_task1/A/form.php
@@ -0,0 +1,26 @@
+
+
+<?php
+    if(isset($_POST['submit'])){
+
+        $name = $_POST['username'];
+        echo " your name is: $name";
+    }
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+</head>
+<body> 
+        <form method="post" action="" enctype="multipart/form-data">
+            Name:   <input type="text" name="username" value="<?php if(isset($_POST['name'])){echo $name;} ?>"/> <br>
+                        <input type="submit" name="submit" value="Submit"/>
+        </form>
+</body>
+</html>
